81be98036709db040a4bbafd76c138b8265a6413
ScribbleGK <gkarc1002@gmail.com>
2025-09-17 21:03:59 +1000

    feat: Manually configure and set up Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-indigo-600 underline">
+        Gestor de Turnos
+      </h1>
+    </div>
+  )
+}
+
+export default App

f53f85ce9ae7be823fa3d3544c1538335c50e517
ScribbleGK <gkarc1002@gmail.com>
2025-10-01 01:03:57 +1000

    Update production API URL in apiConfig.js
    Changed the production API endpoint from 'gestor-de-turnos-api.onrender.com' to 'gkcs.scribbledev.com' to reflect the new deployment location.

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 // Esta variable cambiará automáticamente dependiendo si estamos en desarrollo o producción.
 const apiUrl = import.meta.env.PROD 
-  ? 'https://gestor-de-turnos-api.onrender.com/api' // URL de Producción
+  ? 'https://gkcs.scribbledev.com/api' // URL de Producción
   : 'http://localhost:3001/api'; // URL de Desarrollo
 
 export default apiUrl;
